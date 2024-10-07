377d24fe4743468b544c89e0fbc968e35fa1a3ce
Henry Ofodieze <easybulb@gmail.com>
2024-10-07 19:41:05 +0000

    add function to toggle DEBUG to true or false on/off production

--- mimo/settings.py
+++ mimo/settings.py
@@ -28,7 +28,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if os.path.isfile("env.py"):
+    DEBUG = True
+else:
+    DEBUG = False
 
 ALLOWED_HOSTS = ['8000-vishalchapa-mimo-kq1n7j7avhq.ws.codeinstitute-ide.net', '.herokuapp.com']
 CSRF_TRUSTED_ORIGINS = ['https://*.codeinstitute-ide.net', 'https://*.herokuapp.com']
