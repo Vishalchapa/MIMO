761ee42ef438ad5dbb5949308fb0dbd4c403ce53
Henry Ofodieze <easybulb@gmail.com>
2024-10-08 10:28:35 +0000

    create base.html, index.html and the corr. urls paths and views

--- core/views.py
+++ core/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+class IndexPage(TemplateView):
+    template_name = 'core/index.html'
