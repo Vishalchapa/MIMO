4d94690e683656cc1328999b904ea9f89d403fad
Joe Brookes <joe.brookes@students.codeinstitute.net>
2024-10-09 12:45:38 +0000

    create forms.py in tasks and add forms for model

---
+++ tasks/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from .models import Task
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ['title', 'body', 'priority', 'due_day', 'status']
+        widgets = {
+            'due_day': forms.DateInput(attrs={'type': 'date'}),
+            'status': forms.Select(),  # For dropdown choices
+            'priority': forms.Select(),  # For dropdown priority
+        }
