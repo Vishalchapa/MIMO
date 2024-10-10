27970aefb40997e37c217e2f3e2c1c2b89f8ad29
Anna Sahaidachna <anna.sahaidachna@gmail.com>
2024-10-10 16:13:06 +0000

    Separate edit task from create new task

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const createTask = document.getElementById('createTaskForm')
     const createTaskBtn = document.getElementById('create-task-btn')
-    createTaskBtn.addEventListener('click', function(){
-           createTask.style.display = 'block';
-        })
+    const createTask = document.getElementById('createTaskForm')
+    const editButtons = document.getElementsByClassName("btn-edit");
+    const submitButton = document.getElementById("submitButton");
+
+    if (createTaskBtn && createTask) { // Check if elements exist
+        createTaskBtn.addEventListener('click', function() {
+            createTask.style.display = 'block';
+            this.style.display = 'none'; // Hide the button when clicked
+        });
     }
-);
+
+    for (let button of editButtons) {
+        button.addEventListener("click", (e) => {
+            let taskId = e.target.getAttribute("task_id");
+            let taskContent = document.getElementById(`task${taskId}`).innerText;
+            taskTitle.value = taskContent;
+            submitButton.innerText = "Update";
+            taskForm.setAttribute("action", `edit_task/${taskId}`);
+        });
+    }
+});
