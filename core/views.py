a95724d47fc0469ef3e01ff83f48ba1fe4590c23
Henry Ofodieze <easybulb@gmail.com>
2024-10-10 02:57:27 +0000

    update core views.py with views for about and faq pages

--- core/views.py
+++ core/views.py
@@ -1,4 +1,11 @@
 from django.views.generic import TemplateView
+from django.shortcuts import render
 
 class IndexPage(TemplateView):
     template_name = 'core/index.html'
+
+def about(request):
+    return render(request, 'core/about.html')
+
+def faq(request):
+    return render(request, 'core/faq.html')
