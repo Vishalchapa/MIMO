f728cb678930cd3fb4d9770d87c2421b7d7f7584
Anna Sahaidachna <anna.sahaidachna@gmail.com>
2024-10-10 12:49:51 +0000

    Add icons for actions; change input fields for status and priority

--- tasks/urls.py
+++ tasks/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('task/<int:task_id>/edit/', views.dashboard, name='task-edit'),  # Edit Task
     path('task/<int:task_id>/delete/', views.delete_task, name='task-delete'),  # Delete Task
     path('task/<int:task_id>/complete/', views.mark_as_complete, name='task-complete'),  # Mark as complete
+    path('task/<int:task_id>/change-priority/', views.change_priority, name='change-priority'),
+    path('task/<int:task_id>/change-status/', views.change_status, name='change-status'),
 ]
