075c8a5381087a7fbfeab5e70ef0339b282dc597
Henry Ofodieze <easybulb@gmail.com>
2024-10-11 01:11:54 +0000

    add size attribute in form widget and increase title input width

--- tasks/forms.py
+++ tasks/forms.py
@@ -6,6 +6,7 @@ class TaskForm(forms.ModelForm):
         model = Task
         fields = ['title', 'priority', 'due_day', 'status']
         widgets = {
+            'title': forms.TextInput(attrs={'size': '40', 'placeholder': 'Enter task title'}),
             'due_day': forms.DateInput(attrs={'type': 'date'}),
             'status': forms.Select(),  # For dropdown choices
             'priority': forms.Select(),  # For dropdown priority
