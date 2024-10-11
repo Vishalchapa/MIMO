37e21d87210c5f2040e80c7282ff964146600fa2
Henry Ofodieze <easybulb@gmail.com>
2024-10-11 01:08:39 +0000

    remove body from task form in tasks forms.py

--- tasks/forms.py
+++ tasks/forms.py
@@ -4,7 +4,7 @@ from .models import Task
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
-        fields = ['title', 'body', 'priority', 'due_day', 'status']
+        fields = ['title', 'priority', 'due_day', 'status']
         widgets = {
             'due_day': forms.DateInput(attrs={'type': 'date'}),
             'status': forms.Select(),  # For dropdown choices
